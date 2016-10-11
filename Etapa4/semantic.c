4858b837d1d5d9af6a87c103f1406dd35e471e60
Gabriella Barbieri <gabriellabarbieri@MacBook-Pro-de-Gabriella.local>
2016-10-10 22:18:07 -0300

    Verificação de declarações

--- Etapa4/semantic.c
+++ Etapa4/semantic.c
@@ -16,20 +16,31 @@ void setTypes(TREENODE *node){
 		setTypes(node->child[i]);
 	
 	if(node->type == TREE_DECLARATION_SCALAR){
-		if(node->symbol->type != SYMBOL_LIT_IDENTIFIER){
-			printf("ERRO");
-
+		if(node->symbol->type != SYMBOL_IDENTIFIER){
+			printf("ERRO: Redeclaração de \" %s \", na linha %d. Declarada na linha %d", node->symbol->key, getLineNumber(), node->symbol->lineNumber);
+			exit(4);
 		}
 		else {
 			node->symbol->type 		 = SYMBOL_SCALAR;
 			node->symbol->lineNumber = getLineNumber();
 		}
 	}
-	
-	if(node->type == TREE_DECLARATION_FUC){
-		if(node->symbol->type != SYMBOL_LIT_IDENTIFIER){
-			printf("ERRO");
 
+	if((node->type == TREE_DECLARATION_VEC_LIT) || (node->type == TREE_DECLARATION_VEC_NOLIT)){
+		if(node->symbol->type != SYMBOL_IDENTIFIER){
+			printf("ERRO: Redeclaração de \" %s \", na linha %d. Declarada na linha %d", node->symbol->key, getLineNumber(), node->symbol->lineNumber);
+			exit(4);
+		}
+		else {
+			node->symbol->type  	 = SYMBOL_VECTOR;
+			node->symbol->lineNumber = getLineNumber();
+		}
+	}
+
+	if(node->type == TREE_DECLARATION_FUC){
+		if(node->symbol->type != SYMBOL_IDENTIFIER){
+			printf("ERRO: Redeclaração de \" %s \", na linha %d. Declarada na linha %d", node->symbol->key, getLineNumber(), node->symbol->lineNumber);
+			exit(4);
 		}
 		else {
 			node->symbol->type 		 = SYMBOL_FUNCTION;
