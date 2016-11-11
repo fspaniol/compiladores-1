1d9288b6885fe2fe7a447300c39b832864089d8f
Thiago Bell <thiago@tim>
2016-11-11 21:40:49 -0200

    think its done

--- Etapa5/main.c
+++ Etapa5/main.c
@@ -9,7 +9,7 @@ int main(int argc, char** argv){
         initMe();
         FILE *file;
         if(argc < 2){
-        	printf("Arquivo não informado\n");
+                printf("File not provided\n");
         	exit(1);
         }
         if((argc==2 && (file = fopen(argv[1], "r"))) ) {
@@ -17,9 +17,9 @@ int main(int argc, char** argv){
 		yyparse();
 	}
 	else {
-		printf("Erro na abertura do arquivo!\n");
+                printf("Error opening the file\n");
 		exit(2);
 	}
-	printf("Compilação feita com sucesso!\n");
+        printf("Success!\n");
 	exit(0);
 }
